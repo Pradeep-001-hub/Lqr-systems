98aeebe8dac7298fdbe40ffd7ba1be436220be8a
Pradeep-001-hub <koramutlapradeep@gmail.com>
2026-07-21 11:09:45 +0530

    Update Parameters.m

--- Rocket model/Parameters.m
+++ Rocket model/Parameters.m
@@ -1,1 +1,25 @@
+function P = rocketParameters()
 
+% Physical Properties
+P.mass = 5.0;              % kg
+P.length = 1.5;            % m
+P.diameter = 0.10;         % m
+
+% Moment of inertia (pitch)
+P.Iyy = 0.90;              % kg.m^2
+
+% Gravity
+P.g = 9.81;
+
+% Aerodynamics
+P.Cd = 0.45;
+P.area = pi*(P.diameter/2)^2;
+P.rho = 1.225;
+
+% Thrust
+P.maxThrust = 300;         % N
+
+% TVC
+P.maxGimbal = deg2rad(5);
+
+end
